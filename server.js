28777c85c1428189dbc4485b1ffeb97c53ef9a0e
ebront08 <ebront08@gmail.com>
2021-09-06 15:09:35 -0400

    added commits for the beginning of server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
+// uses express to interact with the front end
 const express = require('express');
+// finds path for filename paths
 const path = require('path');
 const dataBase = require('./db');
+// fs reads and writes to files
 const fs = require('fs');
+// initial port for app listeners
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
