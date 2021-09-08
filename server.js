1bf26994748fb42ad2f9b854fc9a1e3329c337f2
ebront08 <ebront08@gmail.com>
2021-09-08 08:15:03 -0400

    server.js

--- server.js
+++ server.js
@@ -32,3 +32,24 @@ app.post("/api/notes", (req, res) => {
   const newNote = createNote(req.body, dataBase);
   res.json(newNote);
 });
+// This function creates a new note and count ID for each note
+const createNote = (body, notesArray) => {
+  const newNote = body;
+  if (!Array.isArray(notesArray)) notesArray = [];
+  if (notesArray.length === 0) notesArray.push(0);
+
+  body.id = notesArray.length;
+  notesArray[0]++;
+  notesArray.push(newNote);
+
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(notesArray, null, 2)
+  );
+  return newNote;
+};
+
+app.delete("/api/notes/:id", (req, res) => {
+  deleteNote(req.params.id, dataBase);
+  res.json(true);
+});
